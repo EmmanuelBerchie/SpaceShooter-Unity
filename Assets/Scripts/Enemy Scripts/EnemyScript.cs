7c117415838bc2b6f1057d5f3951b266a3f4cb2d
Emmanuel Berchie <emmanuelberchie9@gmail.com>
2019-11-17 02:31:17 +0000

    Added enemy and player sprites
    Bounds for sprites, animation for enemy ships were also added

---
+++ Assets/Scripts/Enemy Scripts/EnemyScript.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    public float speed = 5f;
+    public float rotate_Speed = 50f;
+
+    public bool canShoot;
+    public bool canRotate;
+    private bool canMove = true;
+
+    // Use to manage game bounds of enemy objects 
+    public float bound_X = -11f;
+
+    public Transform attack_Point;
+    public GameObject bulletPrefab;
+
+    private Animator anim;
+    private AudioSource explosionSound; 
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        explosionSound = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        RotateEnemy();
+    }
+
+    void Start()
+    {
+        //Randomise enemy rotations
+        if(canRotate)
+        {
+            if(Random.Range(0,2) > 0)
+            {
+                rotate_Speed = Random.Range(rotate_Speed, rotate_Speed + 20f);
+                rotate_Speed *= -1f;
+            }
+            else
+            {
+                rotate_Speed = Random.Range(rotate_Speed, rotate_Speed + 20f);
+            }
+        }
+    }
+
+    void Move()
+    {
+        if(canMove)
+        {
+            //done to move from the opposite direction of the main player (-=)
+            Vector3 temp = transform.position;
+            temp.x -= speed * Time.deltaTime;
+            transform.position = temp; 
+
+            //Done to deactivate game object 
+            if (temp.x < bound_X)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }//move 
+
+    void RotateEnemy()
+    {
+        if(canRotate)
+        {
+            transform.Rotate(new Vector3(0f, 0f, rotate_Speed * Time.deltaTime), Space.World);
+        }
+    }
+
+}// class
---
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    public float min_Y, max_Y;
+    [SerializeField]
+    private GameObject player_Bullet;
+
+    [SerializeField]
+    private Transform attack_Point;
+    public float attack_Timer = 0.35f;
+    private float current_Attack_Timer;
+    private bool canAttack; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        current_Attack_Timer = attack_Timer; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        Attack();
+    }
+
+    void MovePlayer()
+    {
+        // more than 0 goes up on axis (+)
+        if(Input.GetAxisRaw("Vertical") > 0f)
+        {
+            // transform.position needs to be stored as a variable before it can be used (temp)
+            Vector3 temp = transform.position;
+            temp.y += speed * Time.deltaTime;
+
+            // time. delta time makes the movement of the player smooth
+
+            // if the value is greater than the maximum set the value back to the maximum
+            if (temp.y > max_Y)
+                temp.y = max_Y;
+            transform.position = temp;
+        }
+        // less than 0 goes down on axis (-)
+        else if (Input.GetAxisRaw("Vertical") < 0f)
+        {
+            Vector3 temp = transform.position;
+            temp.y -= speed * Time.deltaTime;
+
+            // if the value is greater than the minumum set the value back to the minumum
+            if (temp.y < min_Y)
+                temp.y = min_Y;
+
+            transform.position = temp;
+        }
+    }
+
+    void Attack()
+    {
+        attack_Timer += Time.deltaTime; 
+        if(attack_Timer > current_Attack_Timer)
+        {
+            canAttack = true; 
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            if (canAttack)
+            {
+                canAttack = false;
+                attack_Timer = 0f;
+
+                Instantiate(player_Bullet, attack_Point.position, Quaternion.identity);
+                
+                // play the sound FX 
+            }
+        }
+
+    
+    }// attack 
+}
+
+//class
